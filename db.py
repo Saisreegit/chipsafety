e4c0a041217a6d1b6c79fb017f2266f9de0c96cc
Saisreegit <saisreekemburu@gmail.com>
2025-05-05 20:26:16 +0530

    db.py

--- db.py
+++ db.py
@@ -1,10 +1,16 @@
-# db.py
-import mysql.connector
+import pymysql
+import os
+from dotenv import load_dotenv
 
-def get_connection():
-    return mysql.connector.connect(
-        host="your-rds-endpoint.amazonaws.com",
-        user="your-username",
-        password="your-password",
-        database="excel_logs"
+# Load database config from .env file
+load_dotenv()
+
+def get_db_connection():
+    return pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        db=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306)),
+        cursorclass=pymysql.cursors.DictCursor
     )
