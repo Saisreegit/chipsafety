2918bad19bd3ba8616baa07b017760d522413254
Saisreegit <saisreekemburu@gmail.com>
2025-05-13 10:24:09 +0530

    db.py

--- db.py
+++ db.py
@@ -1,16 +1,15 @@
-import pymysql
+import mysql.connector
 import os
 from dotenv import load_dotenv
 
-# Load database config from .env file
 load_dotenv()
 
 def get_db_connection():
-    return pymysql.connect(
+    connection = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        db=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306)),
-        cursorclass=pymysql.cursors.DictCursor
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
+    return connection
+
