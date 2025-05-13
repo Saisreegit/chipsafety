79be142d739ba57fda80a0d2ff448099014cb3cc
EC2 Default User <ec2-user@ip-172-31-88-171.ec2.internal>
2025-05-13 11:33:17 +0000

    Updated code for testing

--- test_db.py
+++ test_db.py
@@ -5,4 +5,8 @@ try:
     print("✅ Database connection successful!")
     conn.close()
 except Exception as e:
+<<<<<<< HEAD
     print("❌ Database connection failed:", e)
+=======
+    print(f"❌ Database connection failed: {e}")
+>>>>>>> fccd4c5 (Connected EC2 to RDS and tested DB connection)
