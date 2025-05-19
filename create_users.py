6d11cbe3f3d12a8cd036619443c01d15938909ff
Saisreegit <saisreekemburu@gmail.com>
2025-05-19 12:06:52 +0530

    create_users.py

---
+++ create_users.py
@@ -0,0 +1,7 @@
+from db import insert_user
+
+# Add your users here
+insert_user("admin", "password123")
+insert_user("jane", "securepass1")
+insert_user("john", "hello123")
+insert_user("sree", "sree123")
